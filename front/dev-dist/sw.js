3d316f8f1bb67d572e7871f5d3e434a9f9da0885
Luís Miguel Esquivel <miguelesquivel2018@outlook.com>
2026-01-16 20:23:00 -0300

    asd

--- front/dev-dist/sw.js
+++ front/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c4qj7f0bt2g"
+    "revision": "0.ir86hsg14u8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
