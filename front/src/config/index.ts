a75494916b244fb566fb853f585b2385607a1e7a
Luís Miguel Esquivel <135629204+LMguel@users.noreply.github.com>
2026-02-02 17:47:19 +0000

    nginx

--- front/src/config/index.ts
+++ front/src/config/index.ts
@@ -1,7 +1,7 @@
 // Remove /api do final se existir para evitar duplicação
-// Força o uso do backend local
+// No Codespace, use a URL pública
 export const config = {
-  API_URL: 'https://registra-ponto.duckdns.org',//'http://192.168.1.106:5000'//
+  API_URL: 'https://registra-ponto.duckdns.org',////'http://192.168.1.106:5000'//
   APP_NAME: (import.meta as any).env?.VITE_APP_NAME || 'REGISTRA.PONTO',
   APP_VERSION: (import.meta as any).env?.VITE_APP_VERSION || '1.0.0',
 };
