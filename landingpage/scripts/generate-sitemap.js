f03df0a303e60571c904caed2d28aa8b180d75a7
Luís Miguel Esquivel <miguelesquivel2018@outlook.com>
2026-06-29 12:25:52 -0300

    indexação

--- landingpage/scripts/generate-sitemap.js
+++ landingpage/scripts/generate-sitemap.js
@@ -1,26 +1,23 @@
 import fs from "fs";
 
+const today = new Date().toISOString().split("T")[0];
+
 const urls = [
-  "/",
-  "/#funcionalidades",
-  "/#planos",
-  "/#faq"
+  { path: "/",                  priority: "1.0", changefreq: "weekly" },
+  { path: "/#funcionalidades",  priority: "0.8", changefreq: "monthly" },
+  { path: "/#planos",           priority: "0.9", changefreq: "weekly" },
+  { path: "/#faq",              priority: "0.6", changefreq: "monthly" },
 ];
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
-
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-${urls.map(url=>`
-<url>
-<loc>https://www.registraponto.app.br${url}</loc>
-<changefreq>weekly</changefreq>
-<priority>0.8</priority>
-</url>
-`).join("")}
-
+${urls.map(({ path, priority, changefreq }) => `  <url>
+    <loc>https://www.registraponto.app.br${path}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${priority}</priority>
+  </url>`).join("\n")}
 </urlset>`;
 
 fs.writeFileSync("./public/sitemap.xml", xml);
-
-console.log("Sitemap criado com sucesso");
+console.log("Sitemap criado com sucesso:", today);
